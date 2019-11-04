36fb6dd5d94962a3521500f487d9b5ff500858f9
Pougliche <pugliese.viktor@gmail.com>
2019-11-05 00:13:25 +0100

    yoyo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_LOGIN'],
+    :user_name => 'apikey',
     :password => ENV['SENDGRID_PWD'],
     :domain => 'monsite.fr',
     :address => 'smtp.sendgrid.net',
