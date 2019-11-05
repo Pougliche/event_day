0650233cff4e509ea7ac0eabeb29b5dd8cff728b
Pougliche <pugliese.viktor@gmail.com>
2019-11-05 13:31:29 +0100

    css marche

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
     has_many :attendances
     has_many :events, through: :attendance
