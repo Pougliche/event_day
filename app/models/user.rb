761d74093cf20c240dc40d4d97345752cf66241d
Pougliche <pugliese.viktor@gmail.com>
2019-11-06 11:50:28 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    after_create :welcome_send
     has_many :attendances
     has_many :events, through: :attendance
     has_many :created_events, foreign_key: 'creator_id', class_name: "Event"
     validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please"} 
     validates :encrypted_password, length: { minimum: 6 }, presence: true
     validates :first_name, :last_name, presence:true
-    after_create :welcome_send
+    
 
 
     def welcome_send
